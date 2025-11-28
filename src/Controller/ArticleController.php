9bfcc3e8ebd3bc4b7ac4316f00a5262dfd759ac9
Quentin Macé <quentin.mace.1110@gmail.com>
2025-11-28 21:58:01 +0100

    CS

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ArticleController extends AbstractController
 {
-    #[Route('/article/{id}', name: 'app_article', requirements: ["id" => "\d+"])]
+    #[Route('/article/{id}', name: 'app_article', requirements: ['id' => "\d+"])]
     public function index(Article $article): Response
     {
         return $this->render('article/index.html.twig', [
