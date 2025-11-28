e05426f72124aede83cda99a908fcba2099f1c34
Quentin Macé <quentin.mace.1110@gmail.com>
2025-11-28 21:56:44 +0100

    FEAT: ✨️Add ArticleController and detailed article page template with dynamic routing and product display integration

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ArticleController extends AbstractController
+{
+    #[Route('/article/{id}', name: 'app_article', requirements: ["id" => "\d+"])]
+    public function index(Article $article): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'article' => $article,
+        ]);
+    }
+}
