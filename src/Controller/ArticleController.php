95ed144fbad95f0f6e8ed88439e96d5df3ab6472
Quentin Macé <quentin.mace.1110@gmail.com>
2025-12-04 14:59:43 +0100

    FEAT: ✨️Refactor cart management by extracting logic to CartHandler service in ArticleController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Entity\Order;
 use App\Entity\OrderLine;
+use App\Entity\User;
 use App\Repository\OrderLineRepository;
 use App\Repository\OrderRepository;
+use App\Service\Handler\CartHandler;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -18,7 +20,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ArticleController extends AbstractController
 {
     #[Route('/article/{id}', name: 'app_article', requirements: ['id' => "\d+"])]
-    public function index(Article $article, Request $request, OrderRepository $orderRepository, OrderLineRepository $lineRepository, EntityManagerInterface $entityManager): Response
+    public function index(Article $article, Request $request, CartHandler $cartHandler): Response
     {
         $form = $this->createFormBuilder()
             ->add('quantity', NumberType::class, ['label' => false])
@@ -28,33 +30,11 @@ final class ArticleController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $quantity = $form->getData()['quantity'];
+
+            /** @var User $client */
             $client = $this->getUser();
 
-            $currentOrder = $orderRepository->findOneBy(['client' => $client, 'isComplete' => false]);
-            if (!$currentOrder) {
-                $currentOrder = new Order();
-                $currentOrder->setClient($client);
-                $currentOrder->setIsComplete(false);
-                $currentOrder->setCreationDate(new \DateTime());
-            }
-            $orderLine = $lineRepository->findOneBy(['article' => $article, 'parentOrder' => $currentOrder]);
-            if (!$orderLine) {
-                $orderLine = new OrderLine();
-                $orderLine->setArticle($article);
-                $orderLine->setQuantity(0);
-                $currentOrder->addOrderLine($orderLine);
-            }
-            $orderLine->setQuantity($orderLine->getQuantity() + $quantity);
-            $entityManager->persist($orderLine);
-            if (0 == $orderLine->getQuantity()) {
-                $currentOrder->removeOrderLine($orderLine);
-                $entityManager->remove($orderLine);
-            }
-            $entityManager->persist($currentOrder);
-            if (0 == $currentOrder->getOrderLines()->count()) {
-                $entityManager->remove($currentOrder);
-            }
-            $entityManager->flush();
+            $cartHandler->addToCart($article, $client, $quantity);
 
             return $this->redirectToRoute('admin_order_index');
         }
---
+++ src/Service/Handler/CartHandler.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Service\Handler;
+
+use App\Entity\Article;
+use App\Entity\Order;
+use App\Entity\OrderLine;
+use App\Entity\User;
+use App\Repository\OrderLineRepository;
+use App\Repository\OrderRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+
+class CartHandler
+{
+    public function __construct(
+        private readonly OrderLineRepository $lineRepository,
+        private readonly OrderRepository $orderRepository,
+        private readonly EntityManagerInterface $entityManager
+    )
+    {
+
+    }
+    public function addToCart(Article $article, User $client, int $quantity): void
+    {
+        $currentOrder = $this->orderRepository->findOneBy(['client' => $client, 'isComplete' => false]);
+        if (!$currentOrder) {
+            $currentOrder = $this->buildOrder($client);
+        }
+        $orderLine = $this->lineRepository->findOneBy(['article' => $article, 'parentOrder' => $currentOrder]);
+        if (!$orderLine) {
+            $orderLine = $this->builOrderLine($article, $currentOrder);
+        }
+        $orderLine->setQuantity($orderLine->getQuantity() + $quantity);
+
+        $this->entityManager->persist($orderLine);
+        if (0 == $orderLine->getQuantity()) {
+            $currentOrder->removeOrderLine($orderLine);
+            $this->entityManager->remove($orderLine);
+        }
+        $this->entityManager->persist($currentOrder);
+        if (0 == $currentOrder->getOrderLines()->count()) {
+            $this->entityManager->remove($currentOrder);
+        }
+
+        $this->entityManager->flush();
+    }
+
+    private function buildOrder(User $client): Order
+    {
+        $order = new Order();
+        $order->setClient($client);
+        $order->setIsComplete(false);
+        $order->setCreationDate(new DateTime());
+
+        return $order;
+    }
+
+    private function builOrderLine(Article $article, Order $parentOrder): OrderLine
+    {
+        $orderLine = new OrderLine();
+        $orderLine->setArticle($article);
+        $orderLine->setQuantity(0);
+        $orderLine->setParentOrder($parentOrder);
+
+        return $orderLine;
+    }
+}
