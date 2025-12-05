a802f058423878456e7e1e2830d801868e7a1a0f
Quentin Macé <quentin.mace.1110@gmail.com>
2025-12-05 19:15:11 +0100

    FEAT: ✨️Extend CartHandler with cart management functionality and update CartController and cart view template accordingly

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,21 +2,51 @@
 
 namespace App\Controller;
 
-use App\Repository\OrderRepository;
+use App\Entity\User;
+use App\Service\Handler\CartHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/cart', name: 'app_cart')]
 final class CartController extends AbstractController
 {
-    #[Route('/cart', name: 'app_cart')]
-    public function index(OrderRepository $orderRepository): Response
+    public function __construct(
+        private readonly CartHandler $cartHandler,
+    ) {
+    }
+
+    #[Route('', name: '')]
+    public function show(): Response
     {
+        /* @var User $client */
         $client = $this->getUser();
-        $currentOrder = $orderRepository->findOneBy(['client' => $client, 'isComplete' => false]);
+        $currentOrder = $this->cartHandler->getCart($client);
 
         return $this->render('cart/index.html.twig', [
             'cart' => $currentOrder,
         ]);
+    }
+
+    #[Route('/empty', name: '_empty')]
+    public function emptyCart(): Response
+    {
+        /* @var User $client */
+        $client = $this->getUser();
+
+        $this->cartHandler->emptyCart($client);
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/validate', name: '_validate')]
+    public function validateCart(): Response
+    {
+        /* @var User $client */
+        $client = $this->getUser();
+
+        $this->cartHandler->validateCart($client);
+
+        return $this->redirectToRoute('app_home');
     }
 }
